b8c472a1b9ab792b317a6755246b433fa78c9b70
MrigyanMaharjan <158739817+MrigyanMaharjan@users.noreply.github.com>
2024-03-04 08:26:48 +0545

    Update works.jsx

--- src/components/works.jsx
+++ src/components/works.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Works() {
+const works = ({todos}) => {
+  console.log(todos)
+  let work= document.querySelector("#work");
+
+
+  work= document.querySelector("#work");
+  if(work!==null && todos!=""){
+    console.log(work)
+    let content=`
+    <div id='work' className='h-[20vh] w-[30vw] bg-purple-200 grid grid-cols-3 grid-rows-4 place-items-center'>
+      ${todos}
+    </div>;`
+    console.log(content)
+    work.innerHTML+=content;
+  }
+
   return (
-   he
-  )
+  <div id='work' className='h-[80vh] w-full bg-purple-700 grid grid-cols-3 grid-rows-4 place-items-center'>
+
+  </div>  )
 }
 
-export default Works;
+export default works
