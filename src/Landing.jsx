c3e19b58403c205f877aadec74ea63fe078f336f
MrigyanMaharjan <158739817+MrigyanMaharjan@users.noreply.github.com>
2024-03-04 08:26:21 +0545

    Update Landing.jsx

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -2,28 +2,46 @@ import React, { useState } from 'react'
 import Works from './components/works.jsx'
 
 function Landing() {
+  const [todo,setTodo]= useState("");
+  const [todos,setTodos]= useState("");
+  let val= document.querySelector("#input");
+
+  const handlechange=()=>{
+    val= document.querySelector("#input");
+      setTodo(val.value)
+  }
+
+  const handleadd=()=>{
+  let adder= document.querySelector("#add");
+  setTodos(todo);
+  console.log("glitech")
+  setTodo("")
+  }
+
   return (
     <>
-    <div className=' min-h-[100%] pb-20 w-full bg-slate-500 flex flex-col overflow-x-hidden   '>
+    <div className='h-[30vh] pb-20 w-full bg-purple-600 flex flex-col overflow-hidden '>
        <h1 className='h-10 w-full text-center py-10 font-medium  text-2xl'>
         This is My To Do List
        </h1>
        <div className='flex align-middle justify-center'>
-       <input className='w-[25%] m-5 rounded-2xl text-center' type="text"  placeholder='Type your Work' />
-       <button className=" h-10 w-20 m-5 border-black  rounded-lg border-solid border-4 hover:bg-slate-300 opacity-0.5">ADD</button>
+       <input className='w-[25%] m-5 rounded-2xl text-center' id='input' value={todo} onChange={handlechange} type="text"  placeholder='Type your Work' />
+       <button id='add' type='submit' onClick={handleadd} className=" h-10 w-20 m-5 border-black  rounded-lg border-solid border-4 duration-150 hover:bg-purple-300 opacity-0.5">ADD</button>
      
      </div>
      <div id='shower' className=" w-[100%] h-[90%] grid grid-cols-3 grid-rows-10 gap-y-6 pt-8 gap-x-2">
-     <p className='flex align-middle justify-center w-screen text-xl'>You have no work pending</p>
+    
      </div>
+
      </div>
-     
-    {/* <Works/> */}
+    
+     <Works todos={todos}/>
+   
      </>
 
   )
 }
-console.log("hello");
+
   
 export default Landing;
 
