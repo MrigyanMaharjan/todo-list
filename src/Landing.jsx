01a15ee0d2a4da5adfd7a0d6f6fb68d61e04b446
MrigyanMaharjan <158739817+MrigyanMaharjan@users.noreply.github.com>
2024-03-04 23:12:49 +0545

    Update Landing.jsx

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,55 +1,55 @@
-import React, { useState } from 'react'
-import Works from './components/works.jsx'
+import React, { useState } from 'react';
 
 function Landing() {
-  const [todo,setTodo]= useState("");
-  const [todos,setTodos]= useState("");
-  let val= document.querySelector("#input");
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
 
-  const handlechange=()=>{
-    val= document.querySelector("#input");
-      setTodo(val.value)
-  }
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
 
-  const handleadd=()=>{
-  let adder= document.querySelector("#add");
-  setTodos(todo);
-  console.log("glitech")
-  setTodo("")
-  }
+  const handleAdd = () => {
+    if (todo.trim() !== '') {
+      setTodos([...todos, { work: todo }]);
+      setTodo('');
+    }
+  };
 
   return (
-    <>
-    <div className='h-[30vh] pb-20 w-full bg-purple-600 flex flex-col overflow-hidden '>
-       <h1 className='h-10 w-full text-center py-10 font-medium  text-2xl'>
-        This is My To Do List
-       </h1>
-       <div className='flex align-middle justify-center'>
-       <input className='w-[25%] m-5 rounded-2xl text-center' id='input' value={todo} onChange={handlechange} type="text"  placeholder='Type your Work' />
-       <button id='add' type='submit' onClick={handleadd} className=" h-10 w-20 m-5 border-black  rounded-lg border-solid border-4 duration-150 hover:bg-purple-300 opacity-0.5">ADD</button>
-     
-     </div>
-     <div id='shower' className=" w-[100%] h-[90%] grid grid-cols-3 grid-rows-10 gap-y-6 pt-8 gap-x-2">
-    
-     </div>
-
-     </div>
-    
-     <Works todos={todos}/>
-   
-     </>
-
-  )
+    <div className='overflow-x-hidden'>
+      <div className='h-[35vh] w-screen pb-20 bg-purple-600 flex flex-col overflow-x-hidden '>
+        <h1 className='h-10 w-full text-center py-10 font-medium  text-2xl'>
+          This is My To Do List
+        </h1>
+        <div className='flex overflow-x-hidden align-middle justify-center'>
+          <input
+            className='w-[25%] overflow-x-hidden m-5 rounded-2xl text-center'
+            value={todo}
+            onChange={handleChange}
+            type='text'
+            placeholder='Type your Work'
+          />
+          <button
+            type='button'
+            onClick={handleAdd}
+            className='h-10 w-20 m-5 overflow-x-hidden border-black  rounded-lg border-solid border-4 duration-150 hover:bg-purple-300 opacity-0.5'
+          >
+            ADD
+          </button>
+        </div>
+      </div>
+      <div className='w-screen h-[65vh]  place-items-center grid grid-cols-3 overflow-x-hidden bg-purple-600 border-opacity-0 gap-10 grid-rows-4'>
+        {todos.map((item, index) => (
+          <section
+            key={index}
+            className='bg-purple-300 mt-3 rounded-xl text-center h-[100px] flex overflow-x-hidden items-center justify-center w-[30vw]'
+          >
+            {item.work}
+          </section>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-  
 export default Landing;
-
-
-// getit.addEventListener("submit",()=>{
-// let works= document.getElementsByTagName("input");
-// console.log(works)
-// let list = works.value;
-// console.log(list)
-
-// })
